884e8fe99443c1f8b0cd058386a9fd61c82d28ef
Dhokoh <davidhogarcia@gmail.com>
2023-01-12 17:41:23 -0500

    Front end routing development debugging

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './assets/components/NavbarHeader';
+import NavbarHeader from './assets/components/NavbarHeader';
 
 function App() {
   return (
-    <Navbar />
+    <NavbarHeader />
   );
 }
 
