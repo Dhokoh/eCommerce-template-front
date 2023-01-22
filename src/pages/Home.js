0ff3666dcb83e10d9639b748ef5b8f4dce141bcf
David <davidhogarcia@gmail.com>
2023-01-22 01:57:31 -0500

    Carousel renderizing. New issue: Second carousel element is overlapping first

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,17 @@ import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
 
 
-function Home(){
+function Home() {
     return (
-        <Container>
-            <h1 className='carouselTitle'>All your favourite products in one place</h1>
+        <Container className='carouselContainer justify-content-center'>
+            <h1 className='carouselTitle'>All products in one place</h1>
             <Carousel>
-                <Carousel.Item>
-                    <img src='.../public/images/cdbproduct.jpg' className='carouselElement'></img>
+                <Carousel.Item className='carouselItem'>
+                    <img src={require('../assets/images/cdbproduct.jpg')} style={width:''} className='carouselElement' alt='This should be the picture of a product category' href=''></img>
+                    <Carousel.Caption>
+                        <h4>Medical homeopathic products</h4>
+                    </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Caption>
-                    <h3></h3>
-                </Carousel.Caption>
             </Carousel>
         </Container>
     );
