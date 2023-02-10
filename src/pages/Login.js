630000ebdcabf139bb500e0332e92ca3ac5ef4cc
David <davidhogarcia@gmail.com>
2023-02-09 20:53:53 -0500

    Login method first steps

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,45 @@
 import { Button, FormGroup } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import {useState} from 'react';
 
 function Login(){
+
+    const [inputs, setInputs] = useState({});
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((values)=>({
+            ...values, [name]:value
+        }))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('https://ecommerce5.onrender.com/users', inputs).then((res)=>{
+            console.log(res);
+        })
+    }
+
+
     return (
         <Container className='carouselContainer justify-content-center'>
             <div className='titleFormSeparator'></div>
             <h2 className='loginTitle'>Login</h2>
             <div className='titleFormSeparator'></div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FormGroup>
                     <Form.Label className='formLabel'>
                         Email
                     </Form.Label>
-                    <Form.Control className='loginPageInput' type='email' placeholder='Enter your email'/>
+                    <Form.Control name='email' onChange={handleChange} className='loginPageInput' type='email' placeholder='Enter your email'/>
                     <Form.Text className='text-muted'>
                         <p className='emailFieldParagraph'>*We will never share your passwords with anyone else nor will we ask you about them.</p>
                     </Form.Text>
                 </FormGroup>
                 <FormGroup controlId='formBasicPassword'>
                     <Form.Label className='formLabel'>Password</Form.Label>
-                    <Form.Control className='loginPageInput' type='password' placeholder='Password'></Form.Control>
+                    <Form.Control name='password' onChange={handleChange} className='loginPageInput' type='password' placeholder='Password'></Form.Control>
                 </FormGroup>
                 <Button type='submit' className='loginButton'>
                     Log in
